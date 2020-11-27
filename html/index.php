cab832b0465144f9cc97828f077fed4b743d4a49
lenny_gin <elenaginebra.z@gmail.com>
2020-11-26 19:10:54 -0600

    adding design to containers

--- html/index.php
+++ html/index.php
@@ -7,6 +7,7 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 </head>
 <body>
+<br>
   <div class = "container">
     <div class="jumbotron">
       <h1 class="display-4">SQL Injection</h1>
@@ -14,28 +15,32 @@
       <hr class="my-4">
       <p>Usa un SQL Injection para conseguir el hash para el proyecto final.</p>
     </div>
-    <form action="" method="post">
-  <div class="imgcontainer">
-    <img src="img_avatar2.png" alt="Avatar" class="avatar">
-  </div>
+    <form action="index.php" method="post">
 
   <div class="container">
+  <div class="container" style="background-color:#80c7cd">
+  <br>
+  <br>
+  <br>
     <label for="uname"><b>Username</b></label>
     <input type="text" placeholder="Enter Username" name="uname" required>
-
+    <br>
     <label for="psw"><b>Password</b></label>
     <input type="password" placeholder="Enter Password" name="psw" required>
-
+    <br><br>
     <button type="submit">Login</button>
     <label>
       <input type="checkbox" checked="checked" name="remember"> Remember me
     </label>
-  </div>
-
-  <div class="container" style="background-color:#f1f1f1">
+    <br>
+    <br>
+    <br>
     <button type="button" class="cancelbtn">Cancel</button>
     <span class="psw">Forgot <a href="#">password?</a></span>
+    <br><br>
   </div>
+  </div>
+
 </form> 
  </div>
  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
