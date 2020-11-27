6d2d8bdc45cedde4f1cb275ffcf1e294f80ea235
lenny_gin <elenaginebra.z@gmail.com>
2020-11-26 18:53:57 -0600

    changing database, adding input, fixing details

--- html/index.php
+++ html/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Web PHP-MySQL con Docker</title>
+  <title>SQL Injection</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 </head>
 <body>
@@ -14,38 +14,33 @@
       <hr class="my-4">
       <p>Usa un SQL Injection para conseguir el hash para el proyecto final.</p>
     </div>
-	<table class="table table-striped table-responsive">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Lastname</th>
-          <th>Email</th>
-          <th>Password</th>
-          <th>HASH</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        $conexion = new mysqli("mysql", "root", "secret", "SG");
+    <form action="" method="post">
+  <div class="imgcontainer">
+    <img src="img_avatar2.png" alt="Avatar" class="avatar">
+  </div>
 
-        $cadenaSQL = "select * from s_user";
-        $resultado = $conexion->query($cadenaSQL);
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
 
-        while ($fila = $resultado->fetch_object()) {
-         echo "<tr><td> " .$fila->firstname . 
-         "</td><td>" . $fila->lastname .
-         "</td><td>" . $fila->email .
-         "</td><td>" . $fila->pass .
-         "</td><td>" . $fila->hash .
-         "</td></tr>";
-       }
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
 
-       ?>
-     </tbody>
-   </table>
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form> 
  </div>
  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 </body>
 </html>
+
--- init.sql
+++ init.sql
@@ -15,5 +15,4 @@ INSERT INTO s_user (id, firstname, lastname, pass, email, hash) VALUES (NULL, 'P
 INSERT INTO s_user (id, firstname, lastname, pass, email, hash) VALUES (NULL, 'Zedd', 'Zedding','123123','zedd@zedding.com', '28b8ee38');
 INSERT INTO s_user (id, firstname, lastname, pass, email, hash) VALUES (NULL, 'Insanity', 'Clarity','eeee','clarity@insanity.com', 'sdajlakjsad');
 INSERT INTO s_user (id, firstname, lastname, pass, email, hash) VALUES (NULL, 'Foxes', 'Foxes','tragedy','tragedy@remedy.com', 'duyegdhs76ad');
-INSERT INTO s_user (id, firstname, lastname, pass, email, hash) VALUES (NULL, 'FUCKING', 'FUCKING','FUCKING','FUCKING@DATABASE.com', 'FUCKING_DATABASE');
 COMMIT;
